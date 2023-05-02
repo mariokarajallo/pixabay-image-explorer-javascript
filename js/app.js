2831aae4d5f8e27a15af2505f9a8c1bcffd346a6
mariokarajallo <mariokarajallo92@gmail.com>
2023-05-01 22:27:40 -0400

    feat: adds function mostrarAlerta

--- js/app.js
+++ js/app.js
@@ -8,10 +8,42 @@ window.onload = () => {
 function validarFormulario(e) {
   e.preventDefault();
 
-  const terminoBusqueda = document.querySelector("error");
+  const terminoBusqueda = document.querySelector("#termino").value;
 
   if (terminoBusqueda === "") {
     mostrarAlerta("Agrega un termino de busqueda");
     return;
+  }
+}
+
+function mostrarAlerta(mensaje) {
+  const existeAlerta = document.querySelector(".error");
+
+  if (!existeAlerta) {
+    const alerta = document.createElement("P");
+    alerta.classList.add(
+      "error",
+      "bg-red-100",
+      "border-red-400",
+      "text-red-700",
+      "px-4",
+      "py-3",
+      "rounded",
+      "max-w-lg",
+      "mx-auto",
+      "mt-6",
+      "text-center"
+    );
+
+    alerta.innerHTML = `
+<strong class="font-bold">Error!      </strong>
+<span class="block sm:inline">${mensaje}</span>
+    `;
+
+    formulario.appendChild(alerta);
+
+    setTimeout(() => {
+      alerta.remove();
+    }, 3000);
   }
 }
