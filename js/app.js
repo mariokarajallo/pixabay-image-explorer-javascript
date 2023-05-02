d51189465d478e586e0a708b159fd4e33580f0a0
mariokarajallo <mariokarajallo92@gmail.com>
2023-05-01 23:26:40 -0400

    feat: adds functions mostrarImagenes

--- js/app.js
+++ js/app.js
@@ -52,9 +52,37 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
   const key = "8722925-77095ecbf430e624a938ffd7d";
-  const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+  const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=50`;
 
   fetch(url)
     .then((response) => response.json())
-    .then((result) => mostrarImagenes(result));
+    .then((result) => mostrarImagenes(result.hits));
+}
+
+function mostrarImagenes(imagenes) {
+  // limpiar el html de resultados previos
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
+
+  // iterar sobre el arreglo de imagenes y construir el HTML
+  imagenes.forEach((imagen) => {
+    const { previewURL, likes, views, largeImageURL } = imagen;
+    resultado.innerHTML += `
+<div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+  <div class="bg-white">
+    <img src=${previewURL} alt="imagen previa" class="w-full">    </img>
+    <div class="p-4">
+      <p class="font-bold">${likes}<span class="font-light"> Me gusta</span></p>
+      <p class="font-bold">${views}<span class="font-light"> Veces vista</span></p>
+      <a href=${largeImageURL} class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1" target="_blank" rel="noopener noreferrer"
+      >
+        Ver Imagen
+      </a>
+    </div>
+
+  </div>  
+</div>
+    `;
+  });
 }
